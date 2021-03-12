89c1dc494c84d36cc4a1387f6d01f9b0d5375223
gulinb <g.bogdan22@gmail.com>
2021-03-12 11:49:27 +0200

    hiding stripe API key

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import {config} from '../../config/config'
 
 import StripeCheckout from 'react-stripe-checkout'
 
 const StripeCheckoutButton = ({price}) => {
     const priceForStripe = price * 100
-    const publishbleKey = 'pk_test_51HMFCsJVBOjbuYiqo5BOwY9rwbnNUTFFGYsygw7TyL0puFWQdR6WDaHpIadBWy2PN3tQ18p4xjch8PB808BCCQ5h00q3KdaWzw'
+    const publishbleKey = config.STRIPE_SECRET_KEY
 
 
 const onToken = token => {
@@ -17,7 +18,7 @@ const onToken = token => {
     return (
         <StripeCheckout
             label='Pay Now'
-            name='CRWN Clothing Ltd.'
+            name='Orfevru'
             billingAddress
             shippingAddress
             image='https://svgshare.com/i/CUz.svg'
